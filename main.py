a10ea7f69a95676f19b39a36eb5aa44febc5655b
2009m <2009@gmail.com>
2023-10-28 13:10:16 +0300

    666

--- main.py
+++ main.py
@@ -1,34 +1,92 @@
+import random
 
 class Human:
-
-    def __init__(self, name):
+    def __init__(self, name="Human", job=None, home=None, car=None):
         self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.home = home
+        self.car = car
+
+    def get_home(self):
+        self.home = House()
+
+    def get_auto(self):
+        self.car = Auto(brands_of_car)
+
+    def get_job(self):
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+        self.job = Job(job_list)
+
+    def to_chill(self, House):
+        self.gladness = +10
+        self.mess = +5
+
+class House:
+
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+
+    def to_clean(self, human):
+        self.mess = 0
+        self.gladness = -5
 
 
 
 class Auto:
 
-    def __init__(self, brand):
-        self.brand = brand
-        self.passengers = []
+    def __init__(self, brand_list):
+        self.brand = random.choice(list(brand_list))
+        self.fuel = brand_list[self.brand]["fuel"]
+        self.strength = brand_list[self.brand]["strength"]
+        self.consumption = brand_list[self.brand]["consumption"]
 
-
-    def add_passenger(self, human):
-        self.passengers.append(human)
-
-    def print_passenger_name(self):
-        if self.passengers != []:
-            print(f"Names of {self.brand} passenger")
-            for name in self.passengers:
-                print(name.name)
+    def drive(self):
+        if self.strength > 0 and self.fuel >= self.consumption:
+            self.fuel -= self.consumption
+            self.strength -= 1
+            return True
         else:
-            print(f"No passenger in {self.brand}")
+            print("The cat cannot move!")
+            return False
 
-nick = Human("Nick")
-kate = Human("Kate")
-car = Auto("Mersedes")
+    def to_repair(self):
+        if self.consumption >= 0:
+            self.consumption = +100
+            return True
+        else:
+            return False
 
-car.add_passenger(nick)
-car.add_passenger(kate)
+class Job:
 
-car.print_passenger_name()
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]['salary']
+        self.gladness_less = job_list[self.job]['gladness_less']
+
+
+job_list = {
+    "Java developer": {"salary": 50, "gladness_less": 10},
+    "Python developer": {"salary": 40, "gladness_less": 3},
+    "C++ developer": {"salary": 60, "gladness_less": 25},
+    "Rust developer": {"salary": 70, "gladness_less": 15},
+    }
+
+
+
+
+brands_of_car = {
+    "BMW":{"fuel": 100, "strength": 100, "consumption": 6},
+    "Lada":{"fuel": 50, "strength": 40, "consumption": 10},
+    "Volvo":{"fuel": 80, "strength": 150, "consumption": 8},
+    "Ferrari":{"fuel": 80, "strength": 120, "consumption": 14}
+}
+
+
